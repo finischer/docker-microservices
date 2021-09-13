556026c194d04db2aab234fe10decc0a216acf3e
finischer <niklasfischer0@gmail.com>
2021-09-13 08:51:28 +0200

    Simplify code

--- client/microservice/src/Api/api.js
+++ client/microservice/src/Api/api.js
@@ -10,32 +10,30 @@ const instance = axios.create({
 
 export async function getProducts() {
   return await resolve(
-    instance.get(BASE + "/api/products").then((res) => res.data.products)
+    instance.get("/api/products").then((res) => res.data.products)
   );
 }
 
 export async function getProductById(product_id) {
   return await resolve(
-    instance.get(BASE + `/api/product/${product_id}`).then((res) => res.data)
+    instance.get(`/api/product/${product_id}`).then((res) => res.data)
   );
 }
 
 export async function deleteProductById(product_id) {
   return await resolve(
-    instance.delete(BASE + `/api/product/${product_id}`).then((res) => res.data)
+    instance.delete(`/api/product/${product_id}`).then((res) => res.data)
   );
 }
 
 export async function updateProductById(product_id, product) {
   return await resolve(
-    instance
-      .put(BASE + `/api/product/${product_id}`, product)
-      .then((res) => res.data)
+    instance.put(`/api/product/${product_id}`, product).then((res) => res.data)
   );
 }
 
 export async function addProduct(product) {
   return await resolve(
-    instance.post(BASE + "/api/product/add", product).then((res) => res)
+    instance.post("/api/product/add", product).then((res) => res)
   );
 }
